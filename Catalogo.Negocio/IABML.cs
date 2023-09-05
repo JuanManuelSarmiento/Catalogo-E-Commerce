672a237ac75f72d5258de0fb32c7b95cffda32ff
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2023-09-05 09:10:06 -0300

    Interfaz ABML
    Se creo esta interfaz para darle estructura a la logica de ABML de las clases

---
+++ Catalogo.Negocio/IABML.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo.Negocio
+{
+    interface IABML<T>
+    {
+        void Create(T newEntity);
+        void Delete(T entity);
+        void Update(T entity);
+        List<T> Listar();
+    }
+}
