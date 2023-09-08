afc4d47bd50195b055f5ad18d433943d4ffb5d9a
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2023-09-08 11:24:24 -0300

    Creacion categoria y marca
    Se crearon las clases de negocio para poder ser listadas en el alta del articulo

---
+++ Catalogo.Negocio/CategoriaNegocio.cs
@@ -0,0 +1,57 @@
+﻿using Catalogo.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo.Negocio
+{
+    public class CategoriaNegocio : IABML<Categoria>
+    {
+        public void Add(Categoria newEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Categoria entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Categoria> Listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+            AccesoADatos datos = new AccesoADatos();
+            try
+            {
+                datos.SetConsulta("SELECT Id, Descripcion from CATEGORIAS");
+                datos.EjecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Categoria aux = new Categoria
+                    {
+                        Id = (int)datos.Lector["Id"],
+                        Descripcion = (string)datos.Lector["Descripcion"]
+                    };
+
+                    lista.Add(aux);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+            return lista;
+        }
+
+        public void Update(Categoria entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Catalogo.Negocio/MarcaNegocio.cs
@@ -0,0 +1,57 @@
+﻿using Catalogo.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo.Negocio
+{
+    public class MarcaNegocio : IABML<Marca>
+    {
+        public void Add(Marca newEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Marca entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Marca> Listar()
+        {
+            List<Marca> lista = new List<Marca>();
+            AccesoADatos datos = new AccesoADatos();
+            try
+            {
+                datos.SetConsulta("SELECT Id, Descripcion from MARCAS");
+                datos.EjecutarLectura();
+                while(datos.Lector.Read())
+                {
+                    Marca aux = new Marca
+                    {
+                        Id = (int)datos.Lector["Id"],
+                        Descripcion = (string)datos.Lector["Descripcion"]
+                    };
+
+                    lista.Add(aux);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex; 
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+            return lista;
+        }
+
+        public void Update(Marca entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
