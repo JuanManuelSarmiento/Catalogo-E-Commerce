28fcf5b843bbaa1e172dde6b834168cdac222afa
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2023-09-11 09:52:43 -0300

    Update Imagen.cs

--- Catalogo.Dominio/Imagen.cs
+++ Catalogo.Dominio/Imagen.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,14 @@ namespace Catalogo.Dominio
 {
     public class Imagen
     {
+        [Required]
         public int Id { get; set; } 
-        public int IdArticulo { get; set; } 
+        [Required]
+        public int IdArticulo { get; set; }
+
+        [RegularExpression(@"\bhttps?://\S+\.(png|jpe?g|gif|bmp|webp)\b", ErrorMessage = "No es una URL de imagen válida.")]
+        [StringLength(1000, ErrorMessage = "Ingreso invalido")]
+        [Required]
         public string ImagenUrl { get; set; }
         public override string ToString()
         {
