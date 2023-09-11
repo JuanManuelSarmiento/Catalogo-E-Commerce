df56cfd09f343750bdabe2a53e2d1394b7118380
jroldanzocco <94011523+jroldanzocco@users.noreply.github.com>
2023-09-11 09:51:42 -0300

    Incluidos dataAnnotations para validaciones

--- Catalogo.Dominio/Articulo.cs
+++ Catalogo.Dominio/Articulo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,19 @@ namespace Catalogo.Dominio
     {
         public int Id { get; set; }
         [DisplayName("Código")]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "Este campo solo admite letras y numeros")]
+        [Required]
         public string Codigo { get; set; }
+
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "Este campo solo admite letras y numeros")]
+        [Required]
         public string Nombre { get; set; }
         [DisplayName("Descripción")]
+        [StringLength(150, ErrorMessage = "Maximo 150 caracteres")]
+        [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "Este campo solo admite letras y numeros")]
+        [Required]
         public string Descripcion { get; set; }
         public Marca Marca { get; set; }
         [DisplayName("Categoría")]
